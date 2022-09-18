e2aa04a1f0fd8cee1b5c85ec288243a50a31ae98
Murtaza Rangwala <dev@murtaza.info>
2022-09-18 12:31:42 +0530

    Added basic header and footer

---
+++ components/Banner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Banner = () => (
+  <div className="relative w-full flex items-center z-0 overflow-hidden nft-gradient">Banner</div>
+);
+
+export default Banner;
